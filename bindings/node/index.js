a84ebf36a6c2be1310cf71ea5291f534379698ea
Twarimitswe-Aaron <aarontwarimitswe@gmail.com>
2026-06-24 02:18:40 +0800

    fix: update node binding musl detection logic and regenerate test reports

--- bindings/node/index.js
+++ bindings/node/index.js
@@ -20,7 +20,7 @@ function isMusl() {
       const lddPath = require('child_process').execSync('which ldd').toString().trim()
       return readFileSync(lddPath, 'utf8').includes('musl')
     } catch (e) {
-      return true
+      return false
     }
   } else {
     const { glibcVersionRuntime } = process.report.getReport().header
